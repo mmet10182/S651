69ef9b3482b7a0daed9ae45ea2ce8dd8ad4fc85d
Михаил Маслов <mmet10182@yandex.ru>
2022-11-28 14:44:28 +0300

    Moved ALLOWED_HOSTS in to .env

--- S651/S651/settings.py
+++ S651/S651/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.7.0.60']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
